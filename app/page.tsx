18a45b26fb11c75d63705614bea7e519d17187ae
Sal Samani <sal@milysec.com>
2024-11-20 06:53:49 +1100

    added web3 module install options

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,9 @@ export default function Home() {
       </div>
       <span className="flex flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-7 -mb-12 max-[800px]:mb-12 font-code text-base font-medium">
         <TerminalSquareIcon className="w-5 h-5 mr-1 mt-0.5" />
-        {"npm install @solana/web3.js@rc"}
+        {"npm install @solana/web3.js"}
       </span>
     </div>
+
   );
 }
