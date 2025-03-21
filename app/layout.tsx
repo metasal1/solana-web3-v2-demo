861a2c4ee13e7ec1a24fa1de9a7231dc7717343f
Steven Luscher <steven.luscher@anza.xyz>
2025-03-21 22:13:21 +0000

    Upgrade to `@solana/kit`

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,12 @@ import { OpenPanelComponent } from '@openpanel/nextjs';
 
 export const metadata: Metadata = {
   title: {
-    default: "SolWebKit - Solana Web3.js v2",
-    template: "%s | SolWebKit / Solana Web3.js v2",
+    default: "Solana Kit",
+    template: "%s | Solana Kit",
   },
   metadataBase: new URL("https://solweb3.vercel.app/"),
   description:
-    "Solana Web Kit / Solana Web3.js v2 is a JavaScript library for interacting with the Solana blockchain.It provides a simple and efficient way to interact with the Solana network, including reading and writing to the blockchain, managing accounts, and interacting with smart contracts.",
+    "Solana Kit is a JavaScript library for interacting with the Solana blockchain.It provides a simple and efficient way to interact with the Solana network, including reading and writing to the blockchain, managing accounts, and interacting with smart contracts.",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -29,11 +29,6 @@ export default function Home() {
         <TerminalSquareIcon className="w-5 h-5 mr-1 mt-0.5" />
         {"npm install @solana/kit"}
       </span>
-      <span className="flex flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-7 -mb-12 max-[800px]:mb-12 font-code text-base font-medium">
-        or
-        <TerminalSquareIcon className="w-5 h-5 mr-1 mt-0.5" />
-        {"npm install @solana/web3.js@2"}
-      </span>
     </div>
 
   );
